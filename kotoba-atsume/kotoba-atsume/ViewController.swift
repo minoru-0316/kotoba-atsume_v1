200905ae0c6432e0b169b4d0dea069cffa40ce09
Minoru Edo <medo0316130@gmail.com>
2020-09-08 17:40:15 +0900

    1:リポジトリ作成

---
+++ kotoba-atsume/kotoba-atsume/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kotoba-atsume
+//
+//  Created by Minoru Edo on 2020/09/08.
+//  Copyright © 2020 Minoru Edo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
